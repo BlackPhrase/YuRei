3572ce7418f4aaceaacb9a4bd156eb6d0c7d0c8f
BlackPhrase <BlackPhrase@users.noreply.gitlab.com>
2022-04-20 23:10:31 +0000

    uCore's gtl::(instrusive_ptr, intrusive_base, intrusive_base_functionality) stubs

---
+++ src/uCore/gtl/intrusive_ptr.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace gtl
+{
+
+template <typename Class, typename Base, typename Functionality>
+class intrusive_ptr
+{
+public:
+private:
+};
+
+struct intrusive_base
+{
+	int stub;
+};
+
+class intrusive_default_functionality
+{
+public:
+};
+
+}; // namespace gtl
